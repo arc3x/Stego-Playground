c82c594ce47f8db8ee6cabb956d1f46688dbcc3b
Matthew Slocum <arco000@gmail.com>
2016-04-22 02:45:07 -0700

    decryption module. works.

--- encrypted_stego/dec.c
+++ encrypted_stego/dec.c
@@ -11,71 +11,81 @@ void handleErrors() {
 }
 
 //code from https://wiki.openssl.org/index.php/EVP_Authenticated_Encryption_and_Decryption
-int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *aad,
-	int aad_len, unsigned char *key, unsigned char *iv,
-	unsigned char *ciphertext, unsigned char *tag)
+int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *aad,
+	int aad_len, unsigned char *tag, unsigned char *key, unsigned char *iv,
+	unsigned char *plaintext)
 {
 	EVP_CIPHER_CTX *ctx;
-
 	int len;
-
-	int ciphertext_len;
+	int plaintext_len;
+	int ret;
 
 	/* Create and initialise the context */
 	if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
-    
-	/* Initialise the encryption operation. */
-	if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, NULL, NULL))
+
+	/* Initialise the decryption operation. */
+	if(!EVP_DecryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, NULL, NULL))
 		handleErrors();
 
-	/* Set IV length if default 12 bytes (96 bits) is not appropriate */
-	if(1 != EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_IVLEN, 16, NULL))
+	/* Set IV length. Not necessary if this is 12 bytes (96 bits) */
+	if(!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_IVLEN, 16, NULL))
 		handleErrors();
 
 	/* Initialise key and IV */
-	if(1 != EVP_EncryptInit_ex(ctx, NULL, NULL, key, iv)) handleErrors();
+	if(!EVP_DecryptInit_ex(ctx, NULL, NULL, key, iv)) handleErrors();
 
 	/* Provide any AAD data. This can be called zero or more times as
 	 * required
 	 */
-	if(1 != EVP_EncryptUpdate(ctx, NULL, &len, aad, aad_len))
+	if(!EVP_DecryptUpdate(ctx, NULL, &len, aad, aad_len))
 		handleErrors();
 
-	/* Provide the message to be encrypted, and obtain the encrypted output.
-	 * EVP_EncryptUpdate can be called multiple times if necessary
+	/* Provide the message to be decrypted, and obtain the plaintext output.
+	 * EVP_DecryptUpdate can be called multiple times if necessary
 	 */
-	if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
+	if(!EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
 		handleErrors();
-	ciphertext_len = len;
+	plaintext_len = len;
 
-	/* Finalise the encryption. Normally ciphertext bytes may be written at
-	 * this stage, but this does not occur in GCM mode
+	/* Set expected tag value. Works in OpenSSL 1.0.1d and later */
+	if(!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, 16, tag))
+		handleErrors();
+
+	/* Finalise the decryption. A positive return value indicates success,
+	 * anything else is a failure - the plaintext is not trustworthy.
 	 */
-	if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)) handleErrors();
-	ciphertext_len += len;
-
+	ret = EVP_DecryptFinal_ex(ctx, plaintext + len, &len);
 
 	/* Clean up */
 	EVP_CIPHER_CTX_free(ctx);
 
-	return ciphertext_len;
+	if(ret > 0)
+	{
+		/* Success */
+		plaintext_len += len;
+		return plaintext_len;
+	}
+	else
+	{
+		/* Verify failed */
+		return -1;
+	}
 }
 
 
 int main(int argc, char* argv[]) {
     //check proper usage
     if (argc != 7) {
-        printf("usage: %s -i <plaintext file> -o <ciphertext file> -p <password>\n", argv[0]);
+        printf("usage: %s -i <ciphertext file> -o <plaintext file> -p <password>\n", argv[0]);
         return 0;
     }
     
     //argument containers (& other var decl)
-    unsigned char* plain;
+    int cipher_len;
     unsigned char* cipher;
-    unsigned char* cipherfile;
+    unsigned char* plain;
+    unsigned char* plainfile;
     unsigned char* password;
-    
-    //variable defs
     unsigned char* key = malloc(101);
    
     //get args into their places
@@ -92,21 +102,22 @@ int main(int argc, char* argv[]) {
             long file_size;
             fseek(fp, 0, SEEK_END);
             file_size = ftell(fp);
+            cipher_len = file_size;
             rewind(fp);
             //allocate buffers
             plain = malloc(file_size+1);
             cipher = malloc(file_size+1);
             //copy file to buffer
-            fread(plain, 1, file_size, fp);
+            fread(cipher, 1, file_size, fp);
             //close file
             fclose(fp);            
         }
         //output filename
         else if (!strcmp(argv[i], "-o")) {
             //allocate space for output filename
-            cipherfile = malloc(sizeof(argv[i+1])+1);
+            plainfile = malloc(sizeof(argv[i+1])+1);
             //copy args
-            strcpy(cipherfile, argv[i+1]);
+            strcpy(plainfile, argv[i+1]);
         }
         //password
         else if (!strcmp(argv[i], "-p")) {
@@ -116,10 +127,10 @@ int main(int argc, char* argv[]) {
     }
 
     //dump args for debug
-    //printf("argc: %i\n", argc);    
-    //printf("-i %s", plain);
-    //printf("-o %s\n", cipherfile);
-    //printf("-p %s\n", password);
+    printf("argc: %i\n", argc);    
+    //printf("-i %s", cipher);
+    printf("-o %s\n", plainfile);
+    printf("-p %s\n", password);
     
     //code from https://wiki.openssl.org/index.php/Libcrypto_API
     /* Load the human readable error strings for libcrypto */
@@ -130,6 +141,7 @@ int main(int argc, char* argv[]) {
     OPENSSL_config(NULL);
     //produce a hash from the password to use as a key
     PKCS5_PBKDF2_HMAC_SHA1(password, strlen(password), "", 0, 10000, 100, key);
+    //printf("key: %s\n", key);
     
     //encrypt in GCM mode
     unsigned char aad[] = {
@@ -139,9 +151,11 @@ int main(int argc, char* argv[]) {
     unsigned char iv[] = {
         0x99, 0xaa, 0x3e, 0x68, 0xed, 0x81, 0x73, 0xa0, 0xee, 0xd0, 0x66, 0x84
     };
-    unsigned char tag[] = "";    
-    int cipher_len = encrypt(plain, strlen(plain), aad, sizeof(aad), key, iv, cipher, tag);
+    unsigned char tag[] = "";       
+    int plain_len = decrypt(cipher, strlen(cipher), aad, 0, tag, key, iv, plain);
+    //printf("plain: %s\n", plain);
     
+    /* Clean up */
     /* Removes all digests and ciphers */
     EVP_cleanup();
     /* if you omit the next, a small leak may be left when you make use of the BIO (low level API) for e.g. base64 transformations */
@@ -149,14 +163,14 @@ int main(int argc, char* argv[]) {
     /* Remove error strings */
     ERR_free_strings();
     
-    FILE* fp = fopen(cipherfile, "w");
+    FILE* fp = fopen(plainfile, "w");
     //check proper opening
     if (fp == NULL) {
         printf("error opening input file\n");
         return 0;
     }
-    //write ciphertext file
-    fwrite(cipher, 1, cipher_len, fp);
+    //write plaintext file
+    fwrite(plain, 1, strlen(plain), fp);
     //close file
     fclose(fp);      
     
