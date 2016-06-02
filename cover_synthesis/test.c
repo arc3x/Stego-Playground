3424caf92465e9fefa47feb3836603b057b74916
Matthew Slocum <arco000@gmail.com>
2016-06-02 10:14:56 -0700

    fixed

--- cover_synthesis/test.c
+++ cover_synthesis/test.c
@@ -7,7 +7,7 @@ int main() {
     int r;
     
     //test emb/ext self undo   
-    strcpy(cmd, "./synth -m tst/msg0.txt -o tst/song0.wav");
+    strcpy(cmd, "./synth -i tst/msg0.txt -o tst/song0.wav");
     printf("%s\n", cmd);
     system(cmd);
     strcpy(cmd, "./ext -i tst/song0.wav -o tst/msg0.out"); 
@@ -16,7 +16,7 @@ int main() {
     r = system("diff tst/msg0.txt tst/msg0.out");
     (!r) ? printf("pass\n\n") : printf("fail\n\n");
     
-    strcpy(cmd, "./synth -m tst/msg1.txt -o tst/song1.wav");
+    strcpy(cmd, "./synth -i tst/msg1.txt -o tst/song1.wav");
     printf("%s\n", cmd);
     system(cmd);
     strcpy(cmd, "./ext -i tst/song1.wav -o tst/msg1.out"); 
@@ -25,7 +25,7 @@ int main() {
     r = system("diff tst/msg1.txt tst/msg1.out");
     (!r) ? printf("pass\n\n") : printf("fail\n\n");
     
-    strcpy(cmd, "./synth -m tst/msg2.txt -o tst/song2.wav");
+    strcpy(cmd, "./synth -i tst/msg2.txt -o tst/song2.wav");
     printf("%s\n", cmd);
     system(cmd);
     strcpy(cmd, "./ext -i tst/song2.wav -o tst/msg2.out"); 
